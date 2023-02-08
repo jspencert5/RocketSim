7fec581c29118964032eb5fad144262bebcf5739
Spencer T <jstaintor5@gmail.com>
2023-02-07 22:39:25 -0500

    File Setup

---
+++ simulator/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('./../LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='RocketSimulator',
+    version='0.1.0',
+    description='Simulation for Rockets',
+    long_description=readme,
+    author='Group Name',
+    author_email='email@unf.edu',
+    url='website.com',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)

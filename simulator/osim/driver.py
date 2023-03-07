60f4d77b5e41bb6156e4e58f99f6597d87f97aaf
Shane <112229422+bubbarooski@users.noreply.github.com>
2023-03-07 18:57:11 -0500

    Adding new files for math with drag

--- simulator/osim/driver.py
+++ simulator/osim/driver.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
 import mathFunctions
-import pandas
 
 
 # ------------------------------------------------------------------------------------------------
